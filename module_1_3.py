407e3b69472b94a7f6aa58ad38ffb0b688956c70
Taran10000 <slawa.tihonov@yandex.ru>
2024-08-29 12:40:42 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+from operator import truediv
+
+name= 'Viascheslav'
+print ('Name:', name)
+age= 40
+print ('Age:', age)
+age= age + 5
+print ('New age:',age)
+is_student = True
+print('Is_student:',is_student)
